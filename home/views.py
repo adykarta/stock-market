80c63e2531c8ce2473cc76e0e94306c6e00342f0
prissyazzahra <prissy.azzahra@gmail.com>
2020-12-20 18:43:49 +0700

    fix template

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
@@ -10,3 +11,4 @@ def home(request):
     return render(request, 'home.html', {
         'foo': 'bar',
     }, content_type='html')
+    
