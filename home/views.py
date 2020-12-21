27cad86df09ab7c4c6199842b80f86c88776177b
prissyazzahra <prissy.azzahra@gmail.com>
2020-12-21 17:17:41 +0700

    finish market feature

--- home/views.py
+++ home/views.py
@@ -2,13 +2,30 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-import requests
+from repository.models import StockRepository
+from django.http import JsonResponse
+from repository.db import myClient
+import requests, json
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', {
-        'foo': 'bar',
-    }, content_type='html')
+    response = {
+        "stocks": get_all_data()
+    }
+    # print(response)
+    return render(request, 'home.html', response, content_type='html')
+
+def get_all_data():
+    stockRepo = StockRepository(myClient)
+    response = requests.get('http://coordinator:8098/buckets/Stocks/keys?keys=true')
+    keys = response.json()[u'keys']
+    allStocks = []
+
+    for x in keys:
+        stock_obj = json.loads(stockRepo.get(x))
+        allStocks.append(stock_obj)
+        
+    return allStocks
     
